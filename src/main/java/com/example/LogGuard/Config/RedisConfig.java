36b40cf83ca6ce1f6cea578ed02f71ada7aab84b
Shaunakk09 <shaunakahluwalia@gmail.com>
2023-07-04 18:55:52 +0530

    "Api response time added"

--- src/main/java/com/example/LogGuard/Config/RedisConfig.java
+++ src/main/java/com/example/LogGuard/Config/RedisConfig.java
@@ -1,17 +1,20 @@
 package com.example.LogGuard.Config;
 
-import org.springframework.cache.CacheManager;
+import io.lettuce.core.ClientOptions;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.*;
 
+import java.time.Duration;
 
 
 @Configuration
@@ -19,17 +22,24 @@ import org.springframework.data.redis.serializer.*;
 @EnableAspectJAutoProxy
 public class RedisConfig {
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory(){
+    public LettuceConnectionFactory lettuceConnectionFactory(){
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName("127.0.0.1");
         redisStandaloneConfiguration.setPort(6379);
-        return new JedisConnectionFactory(redisStandaloneConfiguration);
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofMillis(600000L))
+                .clientOptions(ClientOptions.builder().build())
+                .build();
+
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig);
+        connectionFactory.afterPropertiesSet();
+        return connectionFactory;
     }
     @Bean
     public RedisTemplate<String,Object> redisTemplate(){
         RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setDefaultSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
